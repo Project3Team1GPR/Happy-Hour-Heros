619119e47b6ae54f147b99b2e5be0585da5a6e10
gmeyer24 <144490098+gmeyer24@users.noreply.github.com>
2024-03-19 14:01:46 -0500

    set up localstorage correctly so it updates based on each user

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,20 @@
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
+import { useEffect } from 'react';
+import auth from '../utils/auth'
+import { QUERY_GET_ME } from "../utils/queries";
+import {useQuery} from '@apollo/client'
+import { saveCocktailIds, getSavedCocktailIds } from "../utils/localStorage";
 
 const Home = () => {
+
+
+  if(auth.loggedIn()){
+    const {  data } = useQuery(QUERY_GET_ME);
+    const userData = data?.me.savedCocktails || [];
+    const cocktailData =  userData.map((item)=> item.drinkId)
+    saveCocktailIds(cocktailData);
+    console.log(cocktailData);
+  }
+
   return (
     <div className="container">
       <h1>Awesome Project</h1>
